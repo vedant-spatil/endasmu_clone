3901645a8e94e80e43295ea5e0cc48ea8d59d360
Vedant <vedantsudhirpatil@gmail.com>
2024-01-28 14:30:46 +0530

    js added to learn-more

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
+
+// 4 pillars of DOM
+//1. Selection of Element 
+//2. Changing HTML
+//3. Changing CSS
+//4. Event Listner
+
+var a = document.querySelector("#learn-more h4 a")  
+    a.addEventListener("mouseover",function (){       
+    a.textContent="༼ つ ◕_◕ ༽つ"                  
+    a.style.color="lightblue"                     
+    a.style.fontSize="1.3vw"
+})
+
+a.addEventListener("mouseout",function(){
+    a.textContent="Learn More"
+    a.style.color="aliceblue"
+    a.style.fontSize="1vw"
+})
